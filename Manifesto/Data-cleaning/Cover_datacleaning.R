364f6d976ae90f864bf45f7a1e699fdfac4bb67d
Lauren Hallett <hallett@uoregon.edu>
2018-06-20 09:56:41 -0700

    clean the cover data

--- Manifesto/Data-cleaning/Cover_datacleaning.R
+++ Manifesto/Data-cleaning/Cover_datacleaning.R
@@ -1,6 +1,4 @@
-library(plyr); library(dplyr)
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
 library(stringr)
 
 
@@ -9,14 +7,14 @@ library(stringr)
 #########################
 
 ## Import import keys
-vegkey_2015 <- read.csv("ClimVar_cover_specieskey.csv")
-shelterkey <- read.csv("Shelter_key.csv")
+vegkey_2015 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_cover_specieskey_2015.csv")
+shelterkey <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/Shelter_key.csv")
 
 ### Read in 2015 cover data 
-covdat_2015_0 <- read.csv("ClimVar_Cover_20150508.csv", skip=3) %>%
+covdat_2015_0 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_Cover_20150508.csv", skip=3) %>%
   tbl_df()
 
-covdat_compost_2015 <- read.csv("ClimVar_CompostCover_20150611.csv", skip = 3) %>%
+covdat_compost_2015 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_CompostCover_20150611.csv", skip = 3) %>%
   tbl_df()
 
 covdat_2015 <- left_join(covdat_2015_0, covdat_compost_2015) %>%
@@ -57,7 +55,7 @@ rm(vegkey_2015, veg0_2015, covveg_2015, covdat_notes_2015, covdat_2015, covdat_2
 #########################
 
 
-covdat_2016 <- read.csv("ClimVar_Cover_20160426.csv", skip=3) %>%
+covdat_2016 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_Cover_20160426.csv", skip=3) %>%
   tbl_df()
 
 #extract notes
@@ -75,7 +73,7 @@ covveg_2016 <- covdat_2016[-c(1:6),] %>%
   mutate(Plot = as.character(Plot))
 
 #import keys
-vegkey_2016 <- read.csv("ClimVar_cover_specieskey_2016.csv") %>%
+vegkey_2016 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_cover_specieskey_2016.csv") %>%
   tbl_df() %>%
   mutate(Plot = as.character(Plot), Plot2 = as.character(Plot2)) %>%
   mutate(Plot = ifelse(Plot2 != "", Plot2, Plot)) %>%
@@ -95,23 +93,72 @@ veg_2016 <- merge(veg0_2016, shelterkey, all.x = T) %>%
   tbl_df() %>%
   filter(cover!="") %>%
   mutate(cover=as.numeric(cover)) %>%
-  mutate(year = 2016)
+  mutate(year = 2016) %>%
+  mutate(species_name = ifelse(species_name == "Vulpia myorus", "Vulpia myuros", species_name))
 
 sort(unique((veg_2016$species_name))) 
 
 rm(vegkey_2016, veg0_2016, covveg_2016, covdat_notes_2016, covdat_2016)
 
+
+
+#########################
+### 2017 COVER DATA #####
+#########################
+
+# import data
+covdat_2017 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_Cover_20170526.csv") %>%
+  tbl_df() %>%
+  gather(plotname, cover, X1F:X16F, na.rm=T) %>%
+  mutate(plotname=substr(plotname, 2,5)) %>%
+  mutate(plot=extract_numeric(plotname)) %>%
+  mutate(subplot=substr(plotname,2,4), subplot=ifelse(plot > 9, substr(plotname, 3,4), subplot)) %>%
+  mutate(Plot = as.character(Plot)) %>%
+  filter(!Plot%in%c("Percent_rock", "Percent_bare", "Percent_litter", "Litter_depth_cm",
+                    "Percent_grass", "Percent_forb"))
+
+# import keys
+vegkey_2017 <- read.csv("~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_EnteredData/ClimVar_cover_specieskey_2017.csv") %>%
+  tbl_df() %>%
+  mutate(Plot = as.character(Plot), Plot2 = as.character(Plot2)) %>%
+  mutate(Plot = ifelse(Plot2 != "", Plot2, Plot)) %>%
+  select(-Plot2, -ToDelete) %>%
+  mutate(species_name = as.character(species_name))
+
+# merge data with species key
+veg0_2017 <- merge(covdat_2017, vegkey_2017, all.x=T)
+
+
+# check that all species were retained
+length(intersect(vegkey_2017$Plot, veg0_2017$Plot))
+length(unique(veg0_2017$Plot))
+
+
+# merge data with shelter key
+veg_2017 <- merge(veg0_2017, shelterkey, all.x = T) %>%
+  tbl_df() %>%
+  filter(cover!="") %>%
+  mutate(cover=as.numeric(cover)) %>%
+  mutate(year = 2017)
+
+
+rm(vegkey_2017, veg0_2017, covdat_2017)
+
+
 ##################################
 ### COMBINE YEARS, ADD ZEROS #####
 ##################################
 
-dat.cover <- rbind(veg_2015, veg_2016) %>%
+dat.cover <- rbind(veg_2015, veg_2016, veg_2017) %>%
   tbl_df() %>%
   select(-Plot)
 
 vegkey <- dat.cover %>%
   select(species_name, genus, species, func, status) %>%
   unique()
+
+# check for consistent names across years
+unique(dat.cover$species_name)
 
 # add in 0s for absent species
 zerokey <- expand.grid(unique(dat.cover$species_name), unique(dat.cover$plot), unique(dat.cover$subplot), unique(dat.cover$year))
@@ -131,9 +178,9 @@ dat.cover_with0 <- merge(fullmat2, dat.cover, all.x = T) %>%
          func2 = as.factor(func2)) %>%
   filter(subplot != "L")
 
-rm(fullmat1, fullmat2, vegkey, shelterkey, zerokey, veg_2015, veg_2016)
+rm(fullmat1, fullmat2, vegkey, shelterkey, zerokey, veg_2015, veg_2016, veg_2017)
 
-write.csv(dat.cover_with0, "ClimVar_MasterCover_1516.csv")
+write.csv(dat.cover_with0, "~/Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_CleanedData/ClimVar_species-cover.csv")
 
 
 
