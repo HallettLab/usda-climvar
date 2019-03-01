420d72c71881421bd056d51e41abceba0ba5957d
Lina Aoyama <laoyama@uoregon.edu>
2019-02-28 18:27:34 -0800

    Added BNPP v CWM and RaoQ for each trait

--- Manifesto/Lina/belowground_trait_diversity.R
+++ Manifesto/Lina/belowground_trait_diversity.R
@@ -5,7 +5,8 @@ BNPP <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/BNPP/BNPP_Cleane
 library(tidyverse)
 library(FD)
 
-##Species names must be in same order in both files
+###Set up data for FD package###
+#Species names must be in same order in both files
 veg_keys <- veg[,1:7]
 vegdat <- veg[,8:64]
 vegdat1 <- vegdat[, -c(2,6,10,12,14:16,22:25,28:32,34,37:44,46,53:54,57)] #remove all columns that we do not have trait data on
@@ -37,13 +38,13 @@ dim(below_traits) #check the dimension of trait data
 tr <- as.matrix(below_traits[1:nrow(below_traits), 2:ncol(below_traits)])
 row.names(tr) <- below_traits$ID
 
-##calculate trait diversity
+#Now the data is ready to calculate trait diversity
 results <- dbFD(tr, comp, corr="cailliez")
 results <- as.data.frame(results) %>%
   tbl_df()
-Div <- cbind(veg_keys, results) ##combine with keys
+Div <- cbind(veg_keys, results) #combine with keys
 
-##ANOVA Rao's Q with subplot
+###ANOVA Rao's Q ~ subplot
 library(nlme)
 library(ggplot2)
 fit1 <- lme(RaoQ ~ subplot, random=~1|shelterBlock/treatment, Div) 
@@ -53,7 +54,7 @@ ggplot(data = Div, aes(x = subplot, y = RaoQ, fill = subplot)) +
   geom_boxplot() +
   theme_classic()
 
-##combine BNPP
+###Combine BNPP with trait diversity
 #Replace entry "20-Oct" to "10-20" in depth column in the BNPP dataset
 BNPP$depth <- as.character(BNPP$depth) #set depth as character
 BNPP <- BNPP %>%
@@ -69,7 +70,7 @@ BNPP1 <- BNPP %>%
 joined <- Div %>%
   inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock")) 
 
-##Regression BNPP by Rao's Q
+###Regression BNPP by Rao's Q
 ggplot(data = joined, aes(x=RaoQ, y = agg_BNPP, col = subplot)) +
   geom_point() +
   geom_smooth(method = lm, size = 1, se = FALSE, fullrange = TRUE)+
@@ -89,7 +90,7 @@ summary(fit3)
 fit4 <- lm(agg_BNPP ~ RaoQ, grass)
 summary(fit4)
 
-##Regression BNPP by FDiv (functional divergence)
+###Regression BNPP by FDiv (functional divergence)
 ggplot(data = joined, aes(x=FDiv, y = agg_BNPP, col = subplot)) +
   geom_point() +
   geom_smooth(method = lm, size = 1, se = FALSE, fullrange = TRUE)+
@@ -102,15 +103,278 @@ summary(fit6)
 fit7 <- lm(agg_BNPP ~ FDiv, grass)
 summary(fit7)
 
-##Regression BNPP by FRic (functional richness)
+###Regression BNPP by FRic (functional richness)
 ggplot(data = joined, aes(x=FRic, y = agg_BNPP, col = subplot)) +
   geom_point() +
   geom_smooth(method = lm, size = 1, se = FALSE, fullrange = TRUE)+
   theme_classic()
 #linear regression by subplot
-fit5 <- lm(agg_BNPP ~ FRic, both) 
+fit8 <- lm(agg_BNPP ~ FRic, both) 
 summary(fit5) 
-fit6 <- lm(agg_BNPP ~ FRic, forb)
+fit9 <- lm(agg_BNPP ~ FRic, forb)
 summary(fit6)
-fit7 <- lm(agg_BNPP ~ FRic, grass)
+fit10 <- lm(agg_BNPP ~ FRic, grass)
 summary(fit7)
+
+###Relationships bw CWM of five root traits and BNPP
+library(ggpubr)
+p1 <- ggplot(data = joined, aes(x = CWM.Dens, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("CWM Root Density") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+p2 <- ggplot(data = joined, aes(x = CWM.SRLF, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("CWM SRLF") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position= "none")
+
+p3 <- ggplot(data = joined, aes(x = CWM.SRLC, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("CWM SRLC") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position = "none")
+
+p4 <- ggplot(data = joined, aes(x = CWM.DiamC, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("CWM Diameter Coarse") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position = "none")
+
+p5 <- ggplot(data = joined, aes(x = CWM.PropF, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("CWM Proportion of Fine") +
+  labs(y= NULL) +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  theme(legend.position = "none")
+
+figure <- ggarrange(p1, p2, p3, p4, p5,
+          ncol =3, nrow =2, common.legend = TRUE,
+          align = "v",labels = c("A", "B", "C", "D", "E"))
+annotate_figure(figure, 
+                left = text_grob("Belowground biomass production (soil 0-30 cm)", rot = 90))
+
+#Regression BNPP ~ CWM.Dens
+Dens_all <- lm(agg_BNPP ~ CWM.Dens, joined)
+summary(Dens_all) #not significant
+Dens_both<- lm(agg_BNPP ~ CWM.Dens, both) 
+summary(Dens_both) #significant
+Dens_forb <- lm(agg_BNPP ~ CWM.Dens, forb)
+summary(Dens_forb) #not significant
+Dens_grass <- lm(agg_BNPP ~ CWM.Dens, grass)
+summary(Dens_grass) #not significant
+
+#Regression BNPP ~ CWM.SRLF
+SRLF_all <- lm(agg_BNPP ~ CWM.SRLF, joined)
+summary(SRLF_all) #not significant
+SRLF_both<- lm(agg_BNPP ~ CWM.SRLF, both) 
+summary(SRLF_both) #not significant
+SRLF_forb <- lm(agg_BNPP ~ CWM.SRLF, forb)
+summary(SRLF_forb) #not significant
+SRLF_grass <- lm(agg_BNPP ~ CWM.SRLF, grass)
+summary(SRLF_grass) #not significant
+
+#Regression BNPP ~ CWM.SRLC
+SRLC_all <- lm(agg_BNPP ~ CWM.SRLC, joined)
+summary(SRLC_all) #not significant
+SRLC_both<- lm(agg_BNPP ~ CWM.SRLC, both) 
+summary(SRLC_both) #not significant
+SRLC_forb <- lm(agg_BNPP ~ CWM.SRLC, forb)
+summary(SRLC_forb) #not significant
+SRLC_grass <- lm(agg_BNPP ~ CWM.SRLC, grass)
+summary(SRLC_grass) #not significant
+
+#Regression BNPP ~ CWM.DiamC
+DiamC_all <- lm(agg_BNPP ~ CWM.DiamC, joined)
+summary(DiamC_all) #not significant
+DiamC_both<- lm(agg_BNPP ~ CWM.DiamC, both) 
+summary(DiamC_both) #not significant
+DiamC_forb <- lm(agg_BNPP ~ CWM.DiamC, forb)
+summary(DiamC_forb) #not significant
+DiamC_grass <- lm(agg_BNPP ~ CWM.DiamC, grass)
+summary(DiamC_grass) #significant
+
+#Regression BNPP ~ CWM.PropF
+PropF_all <- lm(agg_BNPP ~ CWM.PropF, joined)
+summary(PropF_all) #not significant
+PropF_both<- lm(agg_BNPP ~ CWM.PropF, both) 
+summary(PropF_both) #not significant
+PropF_forb <- lm(agg_BNPP ~ CWM.PropF, forb)
+summary(PropF_forb) #not significant
+PropF_grass <- lm(agg_BNPP ~ CWM.PropF, grass)
+summary(PropF_grass) #not significant
+
+###Calculate Rao's Q of each trait
+Dens_results <- dbFD(tr[,1], comp, corr="cailliez")
+Dens_results <- as.data.frame(Dens_results) %>%
+  tbl_df()
+Dens_Div <- cbind(veg_keys, Dens_results) #combine with keys
+Dens_joined <- Dens_Div %>%
+  inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock")) 
+
+SRLF_results <- dbFD(tr[,4], comp, corr="cailliez")
+SRLF_results <- as.data.frame(SRLF_results) %>%
+  tbl_df()
+SRLF_Div <- cbind(veg_keys, SRLF_results) #combine with keys
+SRLF_joined <- SRLF_Div %>%
+  inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock")) 
+
+SRLC_results <- dbFD(tr[,3], comp, corr="cailliez")
+SRLC_results <- as.data.frame(SRLC_results) %>%
+  tbl_df()
+SRLC_Div <- cbind(veg_keys, SRLC_results) #combine with keys
+SRLC_joined <- SRLC_Div %>%
+  inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock"))
+
+DiamC_results <- dbFD(tr[,2], comp, corr="cailliez")
+DiamC_results <- as.data.frame(DiamC_results) %>%
+  tbl_df()
+DiamC_Div <- cbind(veg_keys, DiamC_results) #combine with keys
+DiamC_joined <- DiamC_Div %>%
+  inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock"))
+
+PropF_results <- dbFD(tr[,5], comp, corr="cailliez")
+PropF_results <- as.data.frame(PropF_results) %>%
+  tbl_df()
+PropF_Div <- cbind(veg_keys, PropF_results) #combine with keys
+PropF_joined <- PropF_Div %>%
+  inner_join(BNPP1, by = c( "plot", "subplot", "treatment", "shelterBlock"))
+
+#Plot BNPP vs Raos Q of each trait
+f1 <- ggplot(data = Dens_joined, aes(x = RaoQ, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("FD Root Density") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+f2 <- ggplot(data = SRLF_joined, aes(x = RaoQ, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("FD SRLF") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+f3 <- ggplot(data = SRLC_joined, aes(x = RaoQ, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("FD SRLC") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+f4 <- ggplot(data = DiamC_joined, aes(x = RaoQ, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("FD Diameter Coarse") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+f5 <- ggplot(data = PropF_joined, aes(x = RaoQ, y = agg_BNPP, col = subplot)) +
+  geom_point() +
+  theme_classic() +
+  xlab("FD Proportion Fine") +
+  geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE)+
+  labs(y= NULL) +
+  theme(legend.position="none")
+
+figure2 <- ggarrange(f1, f2, f3, f4, f5,
+                    ncol =3, nrow =2, common.legend = TRUE,
+                    labels = c("A", "B", "C", "D", "E"))
+annotate_figure(figure2, 
+                left = text_grob("Belowground biomass production (soil 0-30 cm)", rot = 90))
+
+#subset by subplot
+Dens_both <- Dens_joined %>%
+  filter(subplot == "B")
+Dens_forb <- Dens_joined %>%
+  filter(subplot == "F")
+Dens_grass <- Dens_joined %>%
+  filter(subplot == "G")
+SRLF_both <- SRLF_joined %>%
+  filter(subplot == "B")
+SRLF_forb <- SRLF_joined %>%
+  filter(subplot == "F")
+SRLF_grass <- SRLF_joined %>%
+  filter(subplot == "G")
+SRLC_both <- SRLC_joined %>%
+  filter(subplot == "B")
+SRLC_forb <- SRLC_joined %>%
+  filter(subplot == "F")
+SRLC_grass <- SRLC_joined %>%
+  filter(subplot == "G")
+DiamC_both <- DiamC_joined %>%
+  filter(subplot == "B")
+DiamC_forb <- DiamC_joined %>%
+  filter(subplot == "F")
+DiamC_grass <- DiamC_joined %>%
+  filter(subplot == "G")
+PropF_both <- PropF_joined %>%
+  filter(subplot == "B")
+PropF_forb <- PropF_joined %>%
+  filter(subplot == "F")
+PropF_grass <- PropF_joined %>%
+  filter(subplot == "G")
+
+#Regression BNPP ~ Rao's Q for each trait
+FDDens_all <- lm(agg_BNPP ~ RaoQ, Dens_joined)
+summary(FDDens_all) #not significant
+FDDens_both<- lm(agg_BNPP ~ RaoQ, Dens_both) 
+summary(FDDens_both) #not significant
+FDDens_forb <- lm(agg_BNPP ~ RaoQ, Dens_forb)
+summary(FDDens_forb) #not significant
+FDDens_grass <- lm(agg_BNPP ~ RaoQ, Dens_grass)
+summary(FDDens_grass ) #significant
+
+FDSRLF_all <- lm(agg_BNPP ~ RaoQ, SRLF_joined)
+summary(FDSRLF_all) #not significant
+FDSRLF_both<- lm(agg_BNPP ~ RaoQ, SRLF_both) 
+summary(FDSRLF_both) #not significant
+FDSRLF_forb <- lm(agg_BNPP ~ RaoQ, SRLF_forb)
+summary(FDSRLF_forb) #not significant
+FDSRLF_grass <- lm(agg_BNPP ~ RaoQ, SRLF_grass)
+summary(FDSRLF_grass) #not significant
+
+FDSRLC_all <- lm(agg_BNPP ~ RaoQ, SRLC_joined)
+summary(FDSRLC_all) #not significant
+FDSRLC_both<- lm(agg_BNPP ~ RaoQ, SRLC_both) 
+summary(FDSRLC_both) #not significant
+FDSRLC_forb <- lm(agg_BNPP ~ RaoQ, SRLC_forb)
+summary(FDSRLC_forb) #not significant
+FDSRLC_grass <- lm(agg_BNPP ~ RaoQ, SRLC_grass)
+summary(FDSRLC_grass ) #not significant
+
+FDDiamC_all <- lm(agg_BNPP ~ RaoQ, DiamC_joined)
+summary(FDDiamC_all) #not significant
+FDDiamC_both<- lm(agg_BNPP ~ RaoQ, DiamC_both) 
+summary(FDDiamC_both) #not significant
+FDDiamC_forb <- lm(agg_BNPP ~ RaoQ, DiamC_forb)
+summary(FDDiamC_forb) #not significant
+FDDiamC_grass <- lm(agg_BNPP ~ RaoQ, DiamC_grass)
+summary(FDDiamC_grass ) #not significant
+
+FDPropF_all <- lm(agg_BNPP ~ RaoQ, PropF_joined)
+summary(FDPropF_all) #not significant
+FDPropF_both<- lm(agg_BNPP ~ RaoQ, PropF_both) 
+summary(FDPropF_both) #not significant
+FDPropF_forb <- lm(agg_BNPP ~ RaoQ, PropF_forb)
+summary(FDPropF_forb) #not significant
+FDPropF_grass <- lm(agg_BNPP ~ RaoQ, PropF_grass)
+summary(FDPropF_grass ) #not significant
+
+
+
+
