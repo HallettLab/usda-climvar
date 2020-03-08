bb4e54384ceaf99d22f428f217bdb744e594ae64
Lauren Shoemaker <lash1937@gmail.com>
2020-03-08 10:47:57 -0600

    Created Stan Beverton-Holt model

---
+++ Competition/Model-fit/BH_model.stan
@@ -0,0 +1,54 @@
+// Model for within year fecundity of Bromus
+
+data{
+  int<lower = 1> N;
+  int Fecundity[N];
+  vector[N] intra;
+  vector[N] avfa;
+  vector[N] brho;
+  vector[N] esca;
+  vector[N] laca;
+  vector[N] trhi;
+  vector[N] vumy;
+  //  vector[N] dry; 
+  // vector[N] wet;
+}
+
+parameters{
+  real<lower = 0> lambda;
+  real<lower = 0>  alpha_avfa;
+  real<lower = 0>  alpha_brho;
+  real<lower = 0>  alpha_esca;
+  real<lower = 0>  alpha_laca;
+  real<lower = 0>  alpha_trhi;
+  real<lower = 0>  alpha_vumy;
+
+}
+
+model{
+  // create a vector of predictions
+  vector[N] F_hat;
+
+  // set priors
+  alpha_avfa ~ normal(0, 1000);
+  alpha_brho ~ normal(0, 1000);
+  alpha_esca ~ normal(0, 1000);
+  alpha_laca ~ normal(0, 1000);
+  alpha_trhi ~ normal(0, 1000);
+  alpha_vumy ~ normal(0, 1000);
+  lambda ~ gamma(0.001, 0.001);
+
+
+  // implement the biological model
+  for(i in 1:N){
+    F_hat[i] = lambda*intra[i] / (1 + alpha_avfa*avfa[i] + alpha_brho*brho[i]+ alpha_esca*esca[i]+ alpha_laca*laca[i]+ alpha_trhi*trhi[i]+ alpha_vumy*vumy[i]);
+  }
+
+  // calculate the likelihood
+  Fecundity ~ poisson(F_hat);
+}
+
+
+
+
+
---
+++ Competition/Model-fit/BH_model_check.R
@@ -0,0 +1,77 @@
+# generate data
+
+do.BH <- function(lambda, alpha_avfa, alpha_brho, alpha_esca, alpha_laca, alpha_trhi, alpha_vumy,
+                  avfa, brho, esca, laca, trhi, vumy, focals) {
+  Ntp1_avfa <- rpois(length(avfa), lambda*focals[1:50]/
+                       (1+ alpha_avfa * (avfa + focals[1:50])))
+  Ntp1_brho <- rpois(length(brho), lambda*focals[51:100]/
+                       (1+ alpha_avfa * focals[51:100] + alpha_brho * brho))
+  Ntp1_esca <- rpois(length(esca), lambda*focals[101:150]/
+                       (1+ alpha_avfa * focals[101:150] + alpha_esca * esca))
+  Ntp1_laca <- rpois(length(esca), lambda*focals[151:200]/
+                       (1+ alpha_avfa * focals[151:200] + alpha_laca * laca))
+  Ntp1_trhi <- rpois(length(trhi), lambda*focals[201:250]/
+                       (1+ alpha_avfa * focals[201:250] + alpha_trhi * trhi))
+  Ntp1_vumy <- rpois(length(vumy), lambda*focals[251:300]/
+                       (1+ alpha_avfa * focals[251:300] + alpha_vumy * vumy))
+  
+  return(c(Ntp1_avfa, Ntp1_brho, Ntp1_esca, Ntp1_laca, Ntp1_trhi, Ntp1_vumy))
+}
+
+lambda <- 15
+alpha_avfa <- .1
+alpha_brho <- .02
+alpha_esca <- .1
+alpha_laca <- .01
+alpha_trhi <- .04
+alpha_vumy <- .09
+
+focals <- sample.int(3, size=50*6, replace=TRUE)
+avfa_int <- sample.int(200, size=50, replace=TRUE)
+brho_int <- sample.int(200, size=50, replace=TRUE)
+esca_int <- sample.int(200, size=50, replace=TRUE)
+laca_int <- sample.int(200, size=50, replace=TRUE)
+trhi_int <- sample.int(200, size=50, replace=TRUE)
+vumy_int <- sample.int(200, size=50, replace=TRUE)
+
+#avfa is focal here
+intra <- focals
+
+Fecundity <- do.BH(lambda = lambda, alpha_avfa = alpha_avfa, alpha_brho = alpha_brho, 
+                   alpha_esca = alpha_esca, alpha_laca = alpha_laca, alpha_trhi = alpha_trhi, 
+                   alpha_vumy = alpha_vumy, avfa = avfa_int, brho = brho_int, esca = esca_int, 
+                   laca = laca_int, trhi = trhi_int, vumy = vumy_int, focals = focals)
+
+Fecundity <- as.integer(Fecundity)
+
+avfa <- intra
+avfa[1:50] <- avfa[1:50] + avfa_int
+
+brho <- as.integer(rep(0, length(focals)))
+brho[51:100] <- brho_int
+
+esca <- as.integer(rep(0, length(focals)))
+esca[101:150] <- esca_int
+
+laca <- as.integer(rep(0, length(focals)))
+laca[151:200] <- laca_int
+
+trhi <- as.integer(rep(0, length(focals)))
+trhi[201:250] <- trhi_int
+
+vumy <- as.integer(rep(0, length(focals)))
+vumy[251:300] <- vumy_int
+
+# -----------------------------------------
+# Check stan model
+N <- as.integer(300)
+
+library(rstan)
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+
+setwd("/Users/lash1937/Documents/GitHub/Usda-climvar/Competition/Model-fit")
+PrelimFit <- stan(file = "BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+                                                   "esca", "laca", "trhi", "vumy"),
+                  iter = 3000, chains = 1, thin = 1)
+  # control = list(adapt_delta = 0.8, max_treedepth = 10)
