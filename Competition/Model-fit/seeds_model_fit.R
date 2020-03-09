ae765130e0e67b4e24e6ae566806e9a7986af014
Lauren Shoemaker <lash1937@gmail.com>
2020-03-08 18:50:06 -0600

    fitting data

--- Competition/Model-fit/seeds_model_fit.R
+++ Competition/Model-fit/seeds_model_fit.R
@@ -11,7 +11,11 @@ rstan_options(auto_write = TRUE)
 
 setwd("/Users/lash1937/Documents/GitHub/Usda-climvar/Competition/Model-fit")
 
-data <- stemsin.seedsout
+data <- seedsin.seedsout
+
+
+initials <- list(lambda=20, alpha_avfa=.03, alpha_brho=.03, alpha_laca=.03, alpha_vumy=.03)
+initials1<- list(initials, initials, initials)
 # ---------------------------------------------------------------------------------------------
 # avfa model fitting for wet
 
@@ -20,31 +24,21 @@ dat <- subset(dat, falltreatment == "wet")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- avfa
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- avfa
 
-initials <- list(lambda=10, alpha_avfa=0.03, alpha_brho=0.03, alpha_laca=0.03, alpha_vumy=0.03)
-initials1<- list(initials)
-
-stems_avfa_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_avfa_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                    "laca", "vumy"),
-                  iter = 3000, chains = 3, thin = 2, control = list(adapt_delta = 0.8, max_treedepth = 10))
+                  iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                  init = initials1)
 
+save(seeds_avfa_wet, file = "avfa_wet_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # avfa model fitting for dry
 
@@ -53,28 +47,21 @@ dat <- subset(dat, falltreatment == "dry")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- avfa
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- avfa
 
-stems_avfa_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_avfa_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 3000, chains = 3, thin = 2, control = list(adapt_delta = 0.8, max_treedepth = 10))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
+save(seeds_avfa_dry, file = "avfa_dry_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # brho model fitting for wet
 
@@ -83,28 +70,21 @@ dat <- subset(dat, falltreatment == "wet")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- brho
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- brho
 
-stems_brho_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_brho_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 9000, chains = 3, thin = 3, control = list(adapt_delta = 0.95, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
+save(seeds_brho_wet, file = "brho_wet_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # brho model fitting for dry
 
@@ -113,29 +93,21 @@ dat <- subset(dat, falltreatment == "dry")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- brho
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- brho
 
-stems_brho_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_brho_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 9000, chains = 3, thin = 3, control = list(adapt_delta = 0.95, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
-
+save(seeds_brho_dry, file = "brho_dry_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # laca model fitting for dry
 
@@ -144,29 +116,21 @@ dat <- subset(dat, falltreatment == "dry")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- laca
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- laca
 
-stems_laca_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_laca_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 9000, chains = 3, thin = 3, control = list(adapt_delta = 0.95, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
-
+save(seeds_laca_dry, file = "laca_dry_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # laca model fitting for wet
 
@@ -175,29 +139,21 @@ dat <- subset(dat, falltreatment == "wet")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- laca
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- laca
 
-stems_laca_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_laca_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 3000, chains = 3, thin = 1, control = list(adapt_delta = 0.95, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
-
+save(seeds_laca_wet, file = "laca_wet_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # vumy model fitting for wet
 
@@ -206,29 +162,21 @@ dat <- subset(dat, falltreatment == "wet")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- vumy
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- vumy
 
-stems_vumy_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_vumy_wet <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 9000, chains = 3, thin = 3, control = list(adapt_delta = 0.95, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
-
+save(seeds_vumy_wet, file = "vumy_wet_posteriors.rdata")
 # ---------------------------------------------------------------------------------------------
 # vumy model fitting for dry
 
@@ -237,25 +185,18 @@ dat <- subset(dat, falltreatment == "dry")
 
 Fecundity <- as.integer(round(dat$seedsOut))
 
-avfa <- as.integer(dat$AVFA_stemsIn)
-brho <- as.integer(dat$BRHO_stemsIn)
-laca <- as.integer(dat$LACA_stemsIn)
-vumy <- as.integer(dat$VUMY_stemsIn)
+avfa <- as.integer(dat$AVFA_seedsIn)
+brho <- as.integer(dat$BRHO_seedsIn)
+laca <- as.integer(dat$LACA_seedsIn)
+vumy <- as.integer(dat$VUMY_seedsIn)
 
-intra <- vumy
-remove <- which(intra==0)
-
-Fecundity <- Fecundity[-remove]
 N <- as.integer(length(Fecundity))
 
-avfa <- avfa[-remove]
-brho <- brho[-remove]
-laca <- laca[-remove]
-vumy <- vumy[-remove]
-
 intra <- vumy
 
-stems_vumy_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
+seeds_vumy_dry <- stan(file = "Generic_four_species_BH_model.stan", data = c("N", "Fecundity", "intra", "avfa", "brho",
                                                                        "laca", "vumy"),
-                 iter = 9000, chains = 3, thin = 3, control = list(adapt_delta = 0.98, max_treedepth = 20))
+                       iter = 6000, chains = 3, thin = 2, control = list(adapt_delta = 0.95, max_treedepth = 20), 
+                       init = initials1)
 
+save(seeds_vumy_dry, file = "vumy_dry_posteriors.rdata")
