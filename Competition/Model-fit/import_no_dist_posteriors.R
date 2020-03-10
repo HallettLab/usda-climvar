a4c26ea8cafc6f1e9953836a858ae9b0b433d454
Lauren Shoemaker <lash1937@gmail.com>
2020-03-10 11:37:12 -0600

    examining results without the disturbance plots included

--- Competition/Model-fit/import_no_dist_posteriors.R
+++ Competition/Model-fit/import_no_dist_posteriors.R
@@ -6,47 +6,35 @@ library(tidyverse)
 
 # set working directory to project directory, if not already there!
 
-load("./Competition/Model-fit/avfa_dry_posteriors.rdata")
-avfa_dry <- rstan::extract(seeds_avfa_dry)
-stan_dens(seeds_avfa_dry, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_avfa_dry)
+load("./Competition/Model-fit/no_dist_avfa_dry_posteriors.rdata")
+avfa_dry <- rstan::extract(no_dist_seeds_avfa_dry)
+stan_dens(no_dist_seeds_avfa_dry, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
+remove(no_dist_seeds_avfa_dry)
 
-load("./Competition/Model-fit/avfa_wet_posteriors.rdata")
-avfa_wet <- rstan::extract(seeds_avfa_wet)
+load("./Competition/Model-fit/no_dist_avfa_wet_posteriors.rdata")
+avfa_wet <- rstan::extract(no_dist_seeds_avfa_wet)
 stan_dens(seeds_avfa_wet, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_avfa_wet)
+remove(no_dist_seeds_avfa_wet)
 
-load("./Competition/Model-fit/brho_wet_posteriors.rdata")
-brho_wet <- rstan::extract(seeds_brho_wet)
+load("./Competition/Model-fit/no_dist_brho_wet_posteriors.rdata")
+brho_wet <- rstan::extract(no_dist_seeds_brho_wet)
 stan_dens(seeds_brho_wet, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_brho_wet)
+remove(no_dist_seeds_brho_wet)
 
-load("./Competition/Model-fit/brho_dry_posteriors.rdata")
-brho_dry <- rstan::extract(seeds_brho_dry)
+load("./Competition/Model-fit/no_dist_brho_dry_posteriors.rdata")
+brho_dry <- rstan::extract(no_dist_seeds_brho_dry)
 stan_dens(seeds_brho_dry, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_brho_dry)
+remove(no_dist_seeds_brho_dry)
 
-load("./Competition/Model-fit/vumy_dry_posteriors.rdata")
-vumy_dry <- rstan::extract(seeds_vumy_dry)
+load("./Competition/Model-fit/no_dist_vumy_dry_posteriors.rdata")
+vumy_dry <- rstan::extract(no_dist_seeds_vumy_dry)
 stan_dens(seeds_vumy_dry, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_vumy_dry)
+remove(no_dist_seeds_vumy_dry)
 
-load("./Competition/Model-fit/vumy_wet_posteriors.rdata")
-vumy_wet <- rstan::extract(seeds_vumy_wet)
+load("./Competition/Model-fit/no_dist_vumy_wet_posteriors.rdata")
+vumy_wet <- rstan::extract(no_dist_seeds_vumy_wet)
 stan_dens(seeds_vumy_wet, pars = c("lambda", "alpha_avfa", "alpha_brho", "alpha_laca", "alpha_vumy"))
-remove(seeds_vumy_wet)
-
-vumy_dry$lambda <- exp(vumy_dry$lambda)
-vumy_dry$alpha_avfa <- exp(vumy_dry$alpha_avfa)
-vumy_dry$alpha_brho <- exp(vumy_dry$alpha_brho)
-vumy_dry$alpha_laca <- exp(vumy_dry$alpha_laca)
-vumy_dry$alpha_vumy <- exp(vumy_dry$alpha_vumy)
-
-vumy_wet$lambda <- exp(vumy_wet$lambda)
-vumy_wet$alpha_avfa <- exp(vumy_wet$alpha_avfa)
-vumy_wet$alpha_brho <- exp(vumy_wet$alpha_brho)
-vumy_wet$alpha_laca <- exp(vumy_wet$alpha_laca)
-vumy_wet$alpha_vumy <- exp(vumy_wet$alpha_vumy)
+remove(no_dist_seeds_vumy_wet)
 
 # unlist each of the species/rainfall combinations
 vw <- as_tibble(do.call("cbind", vumy_wet)) %>%
