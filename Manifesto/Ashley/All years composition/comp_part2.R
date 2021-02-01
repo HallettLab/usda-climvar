9480f8b5fae0790998aa43b63c1ed43be52659e2
Ashley Shaw <e.ashley.shaw@gmail.com>
2021-02-01 12:38:03 -0800

    #comments for clarification

--- Manifesto/Ashley/All years composition/comp_part2.R
+++ Manifesto/Ashley/All years composition/comp_part2.R
@@ -1,3 +1,9 @@
+#Biodiversity effects across rainfall treatments in CA annual grasslands
+#the purpose of this code is additive partitioning of biodiversity effects in the ClimVar mixed composition plots
+#this code accompanies the partitioning manuscript and reproducible, publication figures are in the final section 
+#Date of last update: 1/February/2021
+#Author: Ashley Shaw
+
 library(tidyverse)
 library(nlme)
 library(ggplot2)
