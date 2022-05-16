c610beed0fd2a241ac1673a8dea453db55682b8a
LinaAoyama <laoyama@uoregon.edu>
2022-05-16 16:26:29 -0700

    update RaoQ and Biomass figure with new data

--- Manifesto/Lina/total_biomass_raoQ.R
+++ Manifesto/Lina/total_biomass_raoQ.R
@@ -1,5 +1,5 @@
 veg <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/Cover/Cover_CleanedData/ClimVar_2015_species-cover_wide.csv", header = TRUE)
-traits <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/Traits/Traits_GHscreening_Brad/BradTraits_Cleaned.csv", header = TRUE)
+traits <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/Traits/Traits_GHscreening_Lina_Ashley/Combined_GHtraits.csv", header = TRUE)
 BNPP <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/BNPP/BNPP_CleanedData/BNPP_MayHarvest_2015.csv", header = TRUE)
 ANPP <- read.csv("./Dropbox/ClimVar/DATA/Plant_composition_data/ANPP/ANPP_CleanedData/ClimVar_ANPP-peak.csv", header = TRUE)
 
@@ -10,65 +10,64 @@ library(FD)
 #Species names must be in same order in both files
 veg_keys <- veg[,1:7]
 vegdat <- veg[,8:64]
-vegdat1 <- vegdat[, -c(2,6,10,12,14:16,22:25,28:32,34,37:44,46,53:54,57)] #remove all columns that we do not have trait data on
-vegdat1$AVESP <- vegdat1$Avena.barbata + vegdat1$Avena.fatua #group some species
+vegdat1 <- vegdat[, -c(6,10,12,15:16,20,22:25,28,31,32,34,36:39,41:44,46,53,57)] #remove all columns that we do not have trait data for and any species that has no cover in 2015
+#group some species
 vegdat1$BRODIA <- vegdat1$Bromus.diandrus + vegdat1$Bromus.sterilis
-vegdat1$EROBOT <- vegdat1$Erodium.botrys + vegdat1$Erodium.cicutarium + vegdat1$Erodium.moschatum
 vegdat1$HORMUR <- vegdat1$Hordeum.marinum + vegdat1$Hordeum.murinum
-vegdat1$TRISP <- vegdat1$Trifolium.dubium + vegdat1$Trifolium.glomeratum + vegdat1$Trifolium.sp. + vegdat1$Trifolium.subterraneum + vegdat1$Trifolium.wildenovii
-vegdat1$VULMYU <- vegdat1$Vulpia.bromoides + vegdat1$Vulpia.myuros
-vegdat2 <- vegdat1[ , -c(2,3,5,8,12:16,19,21,22,24:28)] #remove grouped columns and Lupinus bicolor because it does not occur in any community
-colnames(vegdat2) <- c("ACHMIL", "BRADIS", "BROHOR", "BROMAD", "CENSOL", "CYNDAC", "CYNECH",
-                       "LACSER", "LOLMUL", "TAECAP", "TRIHIR", "AVESP", "BRODIA", 
-                       "EROBOT", "HORMUR", "TRISP", "VULMYU") #rename columns
-vegdat3 <- vegdat2[c("ACHMIL", "AVESP", "BRADIS", "BRODIA", "BROHOR", "BROMAD", 
-                     "CENSOL", "CYNDAC", "CYNECH", "EROBOT", "HORMUR", "LACSER",
-                     "LOLMUL", "TAECAP", "TRIHIR", "TRISP", "VULMYU")] #reorder columns
+vegdat1$TRISP <- vegdat1$Trifolium.dubium + vegdat1$Trifolium.glomeratum + vegdat1$Trifolium.sp. + vegdat1$Trifolium.wildenovii
+vegdat2 <- vegdat1[ , -c(9,6,16,17,24,25,27,29)] #remove grouped columns 
+colnames(vegdat2) <- c("ACHMIL","ANAARV", "AVEBAR", "AVEFAT", "BRADIS", "BROHOR", "BROMAD", "CENSOL","CERGLO", 
+                       "CYNDAC", "CYNECH", "EROBOT", "EROMOS", "HYPGLA", "HYPRAD", 
+                       "LACSER", "LOLMUL","RUMPUL", "TAECAP", "TRIHIR", "TRISUB", "VICSAT", "VULBRO", "VULMYU",
+                       "BRODIA", "HORMUR", "TRISP") #rename columns
+vegdat3 <- vegdat2[c("ACHMIL","ANAARV", "AVEBAR", "AVEFAT", "BRADIS", "BRODIA","BROHOR", "BROMAD", "CENSOL","CERGLO", 
+                     "CYNDAC", "CYNECH", "EROBOT", "EROMOS",  "HORMUR","HYPGLA", "HYPRAD", 
+                     "LACSER", "LOLMUL","RUMPUL", "TAECAP", "TRIHIR", "TRISP", "TRISUB", "VICSAT", "VULBRO", "VULMYU"
+                      )] #reorder columns
 dim(vegdat3) #check the dimension of veg data
 comp <- as.matrix(vegdat3[1:nrow(vegdat3), 1:ncol(vegdat3)])
 
 traits$ID <- as.character(traits$ID) #set ID column as character
-traits$ID <- ifelse(traits$ID == "AVEFAT", "AVESP", traits$ID) #use Avena fatua traits for Avena sp.
 traits$ID <- ifelse(traits$ID == "TRIREP", "TRISP", traits$ID) #use Trifolium repens traits for Trifolium sp. 
 all_traits <- traits %>%
-  filter(ID %in% c("ACHMIL", "AVESP", "BRADIS", "BRODIA", "BROHOR", "BROMAD", 
-                   "CENSOL", "CYNDAC", "CYNECH", "EROBOT", "HORMUR", "LACSER",
-                   "LOLMUL", "TAECAP", "TRIHIR","TRISP", "VULMYU")) %>%
-  select(ID, SLA, LDMC, Ht, Dens, DiamC, SRLC, SRLF, PropF)
+  filter(ID %in% c("ACHMIL","ANAARV", "AVEBAR", "AVEFAT", "BRADIS", "BRODIA","BROHOR", "BROMAD", "CENSOL","CERGLO", 
+                   "CYNDAC", "CYNECH", "EROBOT", "EROMOS",  "HORMUR","HYPGLA", "HYPRAD", 
+                   "LACSER", "LOLMUL","RUMPUL", "TAECAP", "TRIHIR", "TRISP", "TRISUB", "VICSAT", "VULBRO", "VULMYU")) %>%
+  dplyr::select(ID, SLA, LDMC, Ht, Dens, DiamC, SRLC, SRLF, PropF)
 dim(all_traits) #check the dimension of trait data
 tr <- as.matrix(all_traits[1:nrow(all_traits), 2:ncol(all_traits)])
 row.names(tr) <- all_traits$ID
 
 #Calculate ind RaoQ
-SLA_results <- dbFD(tr[,1], comp, corr = "cailliez")
+SLA_results <- dbFD(tr[,2], comp, corr = "cailliez")
 SLA_results <- as.data.frame(SLA_results) %>%
   tbl_df()
 
-LDMC_results <- dbFD(tr[,2], comp, corr = "cailliez")
+LDMC_results <- dbFD(tr[,3], comp, corr = "cailliez")
 LDMC_results <- as.data.frame(LDMC_results) %>%
   tbl_df()
 
-Ht_results <- dbFD(tr[,3], comp, corr = "cailliez")
+Ht_results <- dbFD(tr[,4], comp, corr = "cailliez")
 Ht_results <- as.data.frame(Ht_results) %>%
   tbl_df()
 
-Dens_results <- dbFD(tr[,4], comp, corr="cailliez")
+Dens_results <- dbFD(tr[,5], comp, corr="cailliez")
 Dens_results <- as.data.frame(Dens_results) %>%
   tbl_df()
 
-DiamC_results <- dbFD(tr[,5], comp, corr="cailliez")
+DiamC_results <- dbFD(tr[,6], comp, corr="cailliez")
 DiamC_results <- as.data.frame(DiamC_results) %>%
   tbl_df()
 
-SRLC_results <- dbFD(tr[,6], comp, corr="cailliez")
+SRLC_results <- dbFD(tr[,7], comp, corr="cailliez")
 SRLC_results <- as.data.frame(SRLC_results) %>%
   tbl_df()
 
-SRLF_results <- dbFD(tr[,7], comp, corr="cailliez")
+SRLF_results <- dbFD(tr[,8], comp, corr="cailliez")
 SRLF_results <- as.data.frame(SRLF_results) %>%
   tbl_df()
 
-PropF_results <- dbFD(tr[,8], comp, corr="cailliez")
+PropF_results <- dbFD(tr[,9], comp, corr="cailliez")
 PropF_results <- as.data.frame(PropF_results)%>%
   tbl_df()
 
@@ -86,7 +85,7 @@ colnames(rao_table) <- c("plot", "subplot", "SLARaoQ", "LDMCRaoQ", "HtRaoQ", "De
 ANPP1 <- ANPP %>%
   filter(year == 2015) %>%
   filter(subplot %in% c("B", "F", "G")) %>%
-  select(plot, subplot, weight_g_m)
+  dplyr::select(plot, subplot, weight_g_m)
 
 #Replace entry "20-Oct" to "10-20" in depth column in the BNPP dataset
 BNPP$depth <- as.character(BNPP$depth) #set depth as character
@@ -98,7 +97,7 @@ BNPP <- BNPP %>%
 BNPP1 <- BNPP %>%
   group_by(plot, subplot) %>% #group data
   summarise(agg_BNPP = sum(bmass_g_m2)) %>% #sum BNPP
-  select(plot, subplot, agg_BNPP)
+  dplyr::select(plot, subplot, agg_BNPP)
 
 joined_rao <- rao_table %>%
   left_join(ANPP1, by = c("plot", "subplot")) %>%
@@ -148,28 +147,29 @@ library(ggpubr)
 p1 <- ggplot(joined_rao, aes(x = RaoQ, y = weight_g_m, color = subplot)) +
   geom_point() +
   theme_bw() +
-  labs(y = "ANPP g/m2", x = "Rao's Q", color = "treatment") +
+  labs(y = bquote('ANPP'~(g/m^2)), x = "Rao's Q", color = "Treatment") +
   geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE) +
-  annotate("text", x = 3.5, y = 750, label = "R2 = 0.50", size = 4, color = "#f8766d") +
-  annotate("text", x = 1.5, y = 530, label = "R2 = 0.09", size = 4, color = "#619bff") +
-  annotate("text", x = 7, y = 300, label = "R2 = 0.03", size = 4, color = "#00ba38") +
-  scale_color_discrete(name = "treatment", labels = c("Mixed", "Forb", "Grass")) 
+  annotate("text", x = 4, y = 750, label = "R2 = 0.75, p < 0.01", size = 4, color = "#f8766d") +
+  annotate("text", x = 2.5, y = 370, label = "R2 = 0.09, p = 0.23", size = 4, color = "#619bff") +
+  annotate("text", x = 7, y = 300, label = "R2 = 0.001, p = 0.88", size = 4, color = "#00ba38") +
+  scale_color_discrete(name = "Treatment", labels = c("Mixed", "Forb", "Grass")) 
 
 p2 <- ggplot(joined_rao, aes(x = RaoQ, y = agg_BNPP, color = subplot)) +
   geom_point() +
   theme_bw() +
-  labs(y = "BNPP g/m2 depth 0-30 cm", x = "Rao's Q", color = "treatment") +
+  labs(y = bquote('BNPP'~(g/m^2)), x = "Rao's Q", color = "Treatment") +
   geom_smooth(method = lm, size = 1, se = FALSE, fullrange = FALSE) +
-  annotate("text", x = 2, y = 400, label = "R2 = <0.001", size = 4, color = "#f8766d") +
-  annotate("text", x = 3.8, y = 300, label = "R2 = 0.15", size = 4, color = "#619bff") +
-  annotate("text", x = 7, y = 210, label = "R2 = 0.002", size = 4, color = "#00ba38") +
-  scale_color_discrete(name = "treatment", labels = c("Mixed", "Forb", "Grass")) 
+  annotate("text", x = 2.6, y = 400, label = "R2 = 0.002, p = 0.84", size = 4, color = "#f8766d") +
+  annotate("text", x = 3.4, y = 290, label = "R2 = 0.03, p = 0.47", size = 4, color = "#619bff") +
+  annotate("text", x = 7, y = 210, label = "R2 = 0.02, p = 0.55", size = 4, color = "#00ba38") +
+  scale_color_discrete(name = "Treatment", labels = c("Mixed", "Forb", "Grass")) 
+
 
 ggarrange(p1, p2, ncol = 2, nrow = 1, 
           common.legend = TRUE, legend = "right",
           align = "v",labels = c("a)", "b)"))
 
-#community RaoQ with ANPP and BNPP
+#community RaoQ with ANPP and BNPP 
 fitB <- lm(weight_g_m ~ RaoQ, joined_rao%>%filter(subplot == "B")) 
 summary(fitB) 
 fitF <- lm(weight_g_m ~ RaoQ, joined_rao%>%filter(subplot == "F"))
@@ -184,7 +184,7 @@ summary(fitF)
 fitG <- lm(agg_BNPP ~ RaoQ, joined_rao%>%filter(subplot == "G"))
 summary(fitG)
 
-#ind trait RaoQ with ANPP and BNPP
+#ind trait RaoQ with ANPP and BNPP Table S6
 #Regression BNPP ~ Rao's Q for each trait
 FDSLA_all <- lm(weight_g_m ~ SLARaoQ, joined_rao)
 summary(FDSLA_all) #significant
@@ -198,14 +198,14 @@ summary(FDSLA_grass ) #not significant
 FDLDMC_all <- lm(weight_g_m ~ LDMCRaoQ, joined_rao)
 summary(FDLDMC_all) #not significant
 FDLDMC_both<- lm(weight_g_m ~ LDMCRaoQ, joined_rao%>%filter(subplot == "B")) 
-summary(FDLDMC_both) #significant
+summary(FDLDMC_both) #not significant
 FDLDMC_forb <- lm(weight_g_m ~ LDMCRaoQ, joined_rao%>%filter(subplot == "F"))
 summary(FDLDMC_forb) #not significant
 FDLDMC_grass <- lm(weight_g_m ~ LDMCRaoQ, joined_rao%>%filter(subplot == "G"))
 summary(FDLDMC_grass ) #not significant
 
 FDHt_all <- lm(weight_g_m ~ HtRaoQ, joined_rao)
-summary(FDHt_all) #not significant
+summary(FDHt_all) #significant
 FDHt_both<- lm(weight_g_m ~ HtRaoQ, joined_rao%>%filter(subplot == "B")) 
 summary(FDHt_both) #not significant
 FDHt_forb <- lm(weight_g_m ~ HtRaoQ, joined_rao%>%filter(subplot == "F"))
@@ -220,7 +220,7 @@ summary(FDDens_both) #not significant
 FDDens_forb <- lm(agg_BNPP ~ DensRaoQ, joined_rao%>%filter(subplot == "F"))
 summary(FDDens_forb) #not significant
 FDDens_grass <- lm(agg_BNPP ~ DensRaoQ, joined_rao%>%filter(subplot == "G"))
-summary(FDDens_grass ) #significant
+summary(FDDens_grass ) #not significant
 
 FDSRLF_all <- lm(agg_BNPP ~ SRLFRaoQ, joined_rao)
 summary(FDSRLF_all) #not significant
@@ -258,4 +258,21 @@ summary(FDPropF_forb) #not significant
 FDPropF_grass <- lm(agg_BNPP ~ PropFRaoQ, joined_rao%>%filter(subplot == "G"))
 summary(FDPropF_grass ) #not significant
 
+# Biomass ANPP and BNPP by overall Rao's Q
+RaoANPP_all <- lm(weight_g_m ~ RaoQ, joined_rao)
+summary(RaoANPP_all)
+RaoANPP_both <- lm(weight_g_m ~ RaoQ, joined_rao%>%filter(subplot == "B"))
+summary(RaoANPP_both)
+RaoANPP_forb <- lm(weight_g_m ~ RaoQ, joined_rao%>%filter(subplot == "F"))
+summary(RaoANPP_forb)
+RaoANPP_grass <- lm(weight_g_m ~ RaoQ, joined_rao%>%filter(subplot == "G"))
+summary(RaoANPP_grass)
 
+RaoBNPP_all <- lm(agg_BNPP ~ RaoQ, joined_rao)
+summary(RaoBNPP_all)
+RaoBNPP_both <- lm(agg_BNPP ~ RaoQ, joined_rao%>%filter(subplot == "B"))
+summary(RaoBNPP_both)
+RaoBNPP_forb <- lm(agg_BNPP ~ RaoQ, joined_rao%>%filter(subplot == "F"))
+summary(RaoBNPP_forb)
+RaoBNPP_grass <- lm(agg_BNPP ~ RaoQ, joined_rao%>%filter(subplot == "G"))
+summary(RaoBNPP_grass)
