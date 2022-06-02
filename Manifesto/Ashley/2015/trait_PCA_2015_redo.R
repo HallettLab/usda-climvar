00d9dbe739b05fda7daf2496582183ec82696989
Ashley Shaw <e.ashley.shaw@gmail.com>
2022-06-02 15:05:53 -0700

    add soil moisture figure

--- Manifesto/Ashley/2015/trait_PCA_2015_redo.R
+++ Manifesto/Ashley/2015/trait_PCA_2015_redo.R
@@ -794,6 +794,51 @@ shapiro.test(residuals(mf6a))
 LS1b<-lsmeans(mf6a, ~treatment)
 contrast(LS1b, "pairwise")
 
+
+##soil moisture plot
+source("./Manifesto/Ashley/ClimVar_Decagon_cleaning.R")
+library(lubridate)
+
+dat7 <- dat2 %>%
+  filter(year == 2014 | year == 2015) %>%
+  mutate(date = ymd_hms(date)) %>%
+  mutate(season = ifelse(month%in%c(9:12, 1), "early", "late")) %>%
+  mutate(complete_date = (paste(year, month, day, sep="-")),
+         complete_date = ymd(complete_date)) %>%
+  filter(month%in%c(9:12, 1:5)) %>%
+  #mutate(toremove = ifelse(plot == 6 & subplot == "G", 1, 0)) %>%
+  filter(date < '2015-05-10 18:00:00',
+         subplot != 'C',
+         date > '2014-10-10 22:00:00') %>%
+  group_by(treatment, subplot, shelterBlock, season, complete_date) %>%
+  summarize(sm = mean(sm))
+
+dat8 <- dat7 %>%
+  tbl_df() %>%
+  #mutate(falltreatment = ifelse(treatment =="consistentDry" | treatment == "fallDry", "dry", "wet")) %>%
+  mutate(treatment=ordered(treatment, levels = c( consistentDry="consistentDry", fallDry="fallDry",
+                                                  springDry="springDry", controlRain="controlRain"))) %>%
+  group_by(treatment, complete_date) %>%
+  summarize(sm = mean(sm, na.rm=T)) 
+
+S1<-ggplot(dat8, aes(x=complete_date, y=sm, color = treatment))  + geom_line(lwd = 1) +
+  theme_bw() + 
+  ylim(0,0.57)+
+  scale_x_date(limits = as.Date(c("2014-10-11","2015-05-10")))+
+  scale_color_manual(values = c("sienna", "peachpuff", "lightsteelblue1","royalblue2"), 
+                     guide = guide_legend(title = "Treatment"),
+                     labels=c("Consistent Drought", "Fall Drought", "Spring Drought","Control" )) +
+  theme(strip.background = element_blank(), 
+        text = element_text(size = 12), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank())+ 
+  geom_segment(aes(x = as.Date("2014-10-11"), y = 0.55, xend = as.Date("2015-05-10"), yend = 0.55), color="sienna", size=5.5)+
+  geom_segment(aes(x = as.Date("2014-10-11"), y = 0.53, xend = as.Date("2015-02-01"), yend = 0.53), color="peachpuff", size=5)+
+  geom_segment(aes(x = as.Date("2015-02-01"), y = 0.53, xend = as.Date("2015-05-10"), yend = 0.53), color="lightsteelblue1", size=5)+
+  labs(y = expression(paste("Soil moisture (m"^3, " m"^-3,")")), x = "Day of 2014-2015 growing season") 
+#scale_color_manual(values = c( "indianred3", "dodgerblue2"))
+#ggsave("Competition/Figures/soil-moisture_201617.pdf", width = 8, height = 5)
+S1
+
 ####Extra
 ggplot(gf_prop_forb, aes(x=percentForb, y=agg_BNPP, color=subplot))+
          geom_point()
